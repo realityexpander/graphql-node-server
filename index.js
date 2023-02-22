fbd57e3927207e829b18d2d0fdd394970cc2b09e
Chris Athanas <realityexpander@gmail.com>
2023-02-21 18:47:35 -0600

    refactor for using use case functions

--- index.js
+++ index.js
@@ -40,6 +40,15 @@ let getContinent = (code) => data.continents.find( continent =>
     continent.code === code
   )
 
+let getCountry = (code) => data.countries.find( country =>
+    country.code === code
+  )
+
+let getLanguage = (code) => data.languages.find( language =>
+    language.code === code
+  )
+
+
 // Provide resolver functions for your schema fields
 const resolvers = {
   Query: {
@@ -53,46 +62,34 @@ const resolvers = {
     // languages: () => data.languages,
     language: (parent, args, context, info) => {
       console.log("Query DB language: ", args)
-      let res = data.languages.find(language => language.code === args["code"])
+      let res = getLanguage(args["code"])
       console.log("Query DB language res: ", res)
       return res
     },
     country: (parent, args, context, info) => {
       console.log("Query DB country: ", args)
-      let res = data.countries.find(country => country.code === args["code"])
+      let res = getCountry(args["code"])
       console.log("Query DB country res: ", res)
       return res
     },
     continent: (parent, args, context, info) => {
       console.log("Query DB continent: ", args)
-      // let res = data.continents.find(continent => continent.code === args["code"])
       let res = getContinent(args["code"])
       console.log("Query DB continent res: ", res)
       return res
     }
   },
   Country: {
-    // continent: (parent, args, context, info) => {
-    //   console.log("Country DB continent2:", info.variableValues)
-    //   let res = getContinent(parent.continent.code)
-    //   console.log("Country DB continent2 res:", res)
-    //   return res
-    // },
     continent: (parent, args, context, info) => {
       console.log("Country DB continent:", parent.continent.code)
-      // let res = data.continents.find( continent => 
-      //   continent.code === parent.continent.code
-      // )
       let res = getContinent(parent.continent.code)
       console.log("Country DB continent res:", res)
       return res
     },
     languages: (parent, args, context, info) => {
-      console.log("Country DB languages:", parent.languages)
-      let res = data.languages.filter( language => 
-        parent.languages.find( countryLanguage => 
-          countryLanguage.code === language.code
-        )
+      console.log("Country DB languages:", parent.languages) 
+      let res = parent.languages.map( countryLanguage =>
+        getLanguage(countryLanguage.code)
       )
       console.log("Country DB languages res:", res)
       return res
@@ -119,7 +116,10 @@ const resolvers = {
     },
     countries: (parent, args, context, info) => {
       console.log("Continent DB countries:", parent.code, parent.countries)
-      let res = data.countries.filter(country => country.continent.code === parent.code)
+      // let res = data.countries.filter(country => country.continent.code === parent.code)
+      let res = parent.countries.map( continentCountry =>
+        getCountry(continentCountry.code)
+      )
       console.log("Continent DB countries res:", res)
       return res
     }
